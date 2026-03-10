e0001a764771a6eb244b14037ccf08bce1965e11
SirBlobby <adithcool.5@gmail.com>
2026-03-10 15:10:50 -0400

    Add sudo to teardown script to fix permission issues

--- stop_service.sh
+++ stop_service.sh
@@ -8,7 +8,7 @@ sudo systemctl disable rdc-router.service
 
 echo "Removing the NetworkManager connection..."
 CURRENT_DIR=$(pwd)
-${CURRENT_DIR}/venv/bin/python ${CURRENT_DIR}/teardown_router.py
+sudo ${CURRENT_DIR}/venv/bin/python ${CURRENT_DIR}/teardown_router.py
 
 echo "Hotspot turned off and service disabled."
 echo "If you want to completely uninstall the service file, run:"
